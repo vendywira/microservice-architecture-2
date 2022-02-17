66ee062812b13808424a50447b7298ded372a0d2
vendywira <wayan.wiranatha@tech.jago.com>
2022-02-18 03:16:09 +0700

    add gateway service

---
+++ gateway-service/src/main/java/com/dkatalis/gateway/GatewayServiceApplication.java
@@ -0,0 +1,25 @@
+package com.dkatalis.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public boolean home() {
+		log.info("#start gateway service");
+		return true;
+	}
+}
---
+++ gateway-service/src/test/java/com/dkatalis/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkatalis.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
